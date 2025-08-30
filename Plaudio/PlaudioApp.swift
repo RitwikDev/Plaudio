0d0ea56f7b10c3ad90539d62eb9181754efd97eb
Ritwik Dev <43510943+RitwikDev@users.noreply.github.com>
2025-08-30 17:21:12 -0500

    Initial Commit

---
+++ Plaudio/PlaudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaudioApp.swift
+//  Plaudio
+//
+//  Created by Ritwik Dev on 30/08/25.
+//
+
+import SwiftUI
+
+@main
+struct PlaudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlaudioTests/PlaudioTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlaudioTests.swift
+//  PlaudioTests
+//
+//  Created by Ritwik Dev on 30/08/25.
+//
+
+import Testing
+@testable import Plaudio
+
+struct PlaudioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
